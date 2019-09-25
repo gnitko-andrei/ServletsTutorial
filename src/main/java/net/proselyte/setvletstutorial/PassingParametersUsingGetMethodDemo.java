48d618762fdddd9eef16e5889f096a8c397e5f02
Andrei <gnitkoandrei17@gmail.com>
2019-09-25 13:42:53 +0300

    add PassingParametersUsingGetMethodDemo and PassingParametersUsingPostMethodDemo

---
+++ src/main/java/net/proselyte/setvletstutorial/PassingParametersUsingGetMethodDemo.java
@@ -0,0 +1,35 @@
+package net.proselyte.setvletstutorial;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class PassingParametersUsingGetMethodDemo extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter writer = response.getWriter();
+        String title = "Passing parameters using GET method";
+
+        String docType = "<!DOCTYPE html>";
+
+        writer.println(
+                "<html>\n" +
+                        "<head><title>" + title + "</title></head>" +
+                        "<body><h2>Speciality: </h2>" +
+                        request.getParameter("speciality") +
+                        "<h2>Experience: </h2>" +
+                        request.getParameter("experience") +
+                        "</body>\n" + "</html>"
+
+        );
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+}

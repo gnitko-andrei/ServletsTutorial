07fff7f3ac5b3be6631b07b8c3878cfc1c5d3b7f
Andrei <gnitkoandrei17@gmail.com>
2019-09-25 23:24:28 +0300

    add FilterDemo

---
+++ src/main/java/net/proselyte/setvletstutorial/FilterDemo.java
@@ -0,0 +1,29 @@
+package net.proselyte.setvletstutorial;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.Date;
+
+public class FilterDemo implements Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        String ipAdress = request.getRemoteAddr();
+        String dateTime = new Date().toString();
+
+        System.out.println("\n\n==============================================\n");
+
+        System.out.println("Request");
+        System.out.println("Date/Time: " + dateTime);
+        System.out.println("IP: " + ipAdress);
+
+        System.out.println("\n\n==============================================\n");
+        filterChain.doFilter(request, response);
+    }
+
+    public void destroy() {
+
+    }
+}
